8230bf734df8f3e0d677ce6f0034ee660dd0393d
Ryan Price <ryanpricelondon@gmail.com>
2022-07-06 21:48:44 -0400

    placeholders for icons and text

--- src/App.js
+++ src/App.js
@@ -1,23 +1,38 @@
-import logo from './logo.svg';
 import './App.css';
+import {Container, Row, Col} from 'react-bootstrap';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div id="main-box" className="mt-5">
+        <Container fluid="lg">
+          <Row className="justify-content-center">
+            <Col>
+              <span>Priceman614</span><br/>
+              0x343...3434
+            </Col>
+            <Col>
+              <img src="https://zoombies.world/images/gold_vip.svg" alt="" width="16%" />
+              <span> 12</span>
+            </Col>
+            <Col>
+              <img src="https://zoombies.world/images/mr-icon.png" alt="" width="16%" />
+              <span> Moonriver Network<br/>465 PLAYERS ONLINE</span>
+            </Col>
+            <Col>
+              <img src="https://zoombies.world/images/zoombies_coin.svg" alt="" width="16%" />
+              <span> 45,000,455</span>
+            </Col>
+            <Col>
+              <img src="https://zoombies.world/images/mr-icon.png" alt="" width="16%" />
+              <span> 1.345</span>
+            </Col>
+            <Col>
+              <span>Chat</span>
+            </Col>
+          </Row>
+        </Container>
+      </div>
     </div>
   );
 }
